07d4deee1ebd1ce188d0a4c565cc8e2b7212fba6
roma <romazabelin1991@gmail.com>
2019-11-06 21:48:44 +0200

    botman config for telegram

---
+++ config/botman/telegram.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Telegram Token
+    |--------------------------------------------------------------------------
+    |
+    | Your Telegram bot token you received after creating
+    | the chatbot through Telegram.
+    |
+    */
+    'token' => env('703953909:AAGNrH2u0s2W-90di4gvwqDh-ATP7YR-mEc'),
+];
